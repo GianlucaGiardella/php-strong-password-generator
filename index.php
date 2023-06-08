86f9a60c12872d34c4497b236bfd3e9e4415c52d
GianluGiarde <gianlugiarde@gmail.com>
2023-06-08 16:12:23 +0200

    Add: index php into logica

--- index.php
+++ index.php
@@ -1,23 +1,4 @@
-<?php
-
-$password_lenght = isset($_GET['password_lenght']) ? $_GET['password_lenght'] : false;
-
-$no_parameters_alert = "Inserisci un parametro";
-
-function generate_password($lenght)
-{
-    $password = "";
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-    for ($i = 0; $i < $lenght; $i++) {
-        $index = rand(0, strlen($characters) - 1);
-        $password .= $characters[$index];
-    }
-
-    return trim($password, " ");
-};
-
-?>
+<?php include_once __DIR__ . '/logica.php'; ?>
 
 <!DOCTYPE html>
 <html lang="en">
